84c611f1602fbf1f35e060f99c59026306a6356f
arnauddlbghe <arnauddelberghe@gmail.com>
2020-10-09 10:02:54 +0200

    ajout bdd

--- ad_symfony_sport.sql
+++ ad_symfony_sport.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  ven. 02 oct. 2020 à 07:48
+-- Généré le :  ven. 09 oct. 2020 à 08:01
 -- Version du serveur :  5.7.21
 -- Version de PHP :  7.2.4
 
@@ -33,18 +33,48 @@ CREATE TABLE IF NOT EXISTS `adherent` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `date` date NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `categorie_id` int(11) DEFAULT NULL,
+  `ville_id` int(11) DEFAULT NULL,
+  `club_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_90D3F060BCF5E72D` (`categorie_id`),
+  KEY `IDX_90D3F060A73F0036` (`ville_id`),
+  KEY `IDX_90D3F06061190A32` (`club_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Déchargement des données de la table `adherent`
 --
 
-INSERT INTO `adherent` (`id`, `nom`, `date`) VALUES
-(2, 'Jean', '2017-09-16'),
-(3, 'Gertrude', '2000-10-10'),
-(4, 'Florian', '2015-01-01'),
-(5, 'Florian', '2021-04-05');
+INSERT INTO `adherent` (`id`, `nom`, `date`, `categorie_id`, `ville_id`, `club_id`) VALUES
+(2, 'Jean', '2017-09-16', 1, 5, 1),
+(3, 'Gertrude', '2000-10-10', 2, NULL, 2),
+(4, 'Florian', '2015-01-01', 3, 2, 2),
+(5, 'Florian', '2021-04-05', 1, 2, 2),
+(6, 'Florian', '1915-01-01', 2, 3, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `adherent_competition`
+--
+
+DROP TABLE IF EXISTS `adherent_competition`;
+CREATE TABLE IF NOT EXISTS `adherent_competition` (
+  `adherent_id` int(11) NOT NULL,
+  `competition_id` int(11) NOT NULL,
+  PRIMARY KEY (`adherent_id`,`competition_id`),
+  KEY `IDX_3495E35F25F06C53` (`adherent_id`),
+  KEY `IDX_3495E35F7B39D312` (`competition_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `adherent_competition`
+--
+
+INSERT INTO `adherent_competition` (`adherent_id`, `competition_id`) VALUES
+(6, 2),
+(6, 3);
 
 -- --------------------------------------------------------
 
@@ -57,7 +87,40 @@ CREATE TABLE IF NOT EXISTS `categorie` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `libelle` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `categorie`
+--
+
+INSERT INTO `categorie` (`id`, `libelle`) VALUES
+(1, 'Divers'),
+(2, 'Combat'),
+(3, 'Aquatique'),
+(4, 'Jeux videos');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `club`
+--
+
+DROP TABLE IF EXISTS `club`;
+CREATE TABLE IF NOT EXISTS `club` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ville_id` int(11) DEFAULT NULL,
+  `nom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_B8EE3872A73F0036` (`ville_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `club`
+--
+
+INSERT INTO `club` (`id`, `ville_id`, `nom`) VALUES
+(1, 4, 'Club de leers'),
+(2, 3, 'Club de roubaix');
 
 -- --------------------------------------------------------
 
@@ -72,7 +135,17 @@ CREATE TABLE IF NOT EXISTS `competition` (
   `date` date NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `competition`
+--
+
+INSERT INTO `competition` (`id`, `libelle`, `date`, `name`) VALUES
+(1, 'crtdfbijdngougbrkyhgbeugbrgbyr', '2020-10-13', 'Competition01'),
+(2, 'vjehubfgusrgbusurfyryg', '2020-10-27', 'Competition02'),
+(3, 'gjegbeyigbzeyfgveyfveyfgve', '2020-10-11', 'Competition03'),
+(4, 'njkzguegbuefheudjrpf', '2020-10-24', 'Competition04');
 
 -- --------------------------------------------------------
 
@@ -94,7 +167,11 @@ CREATE TABLE IF NOT EXISTS `doctrine_migration_versions` (
 
 INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
 ('DoctrineMigrations\\Version20200914084524', '2020-09-14 08:46:15', 113),
-('DoctrineMigrations\\Version20200928095805', '2020-09-28 09:58:27', 680);
+('DoctrineMigrations\\Version20200928095805', '2020-09-28 09:58:27', 680),
+('DoctrineMigrations\\Version20201002075323', '2020-10-02 07:54:08', 1340),
+('DoctrineMigrations\\Version20201009063945', '2020-10-09 06:39:50', 1428),
+('DoctrineMigrations\\Version20201009071336', '2020-10-09 07:13:50', 1493),
+('DoctrineMigrations\\Version20201009074701', '2020-10-09 07:47:14', 1057);
 
 -- --------------------------------------------------------
 
@@ -110,6 +187,56 @@ CREATE TABLE IF NOT EXISTS `entrainement` (
   `heurefin` time NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ville`
+--
+
+DROP TABLE IF EXISTS `ville`;
+CREATE TABLE IF NOT EXISTS `ville` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `libelle` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `ville`
+--
+
+INSERT INTO `ville` (`id`, `libelle`) VALUES
+(1, 'Hem'),
+(2, 'Lille'),
+(3, 'Roubaix'),
+(4, 'Leers'),
+(5, 'Toufflers'),
+(6, 'Lens');
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `adherent`
+--
+ALTER TABLE `adherent`
+  ADD CONSTRAINT `FK_90D3F06061190A32` FOREIGN KEY (`club_id`) REFERENCES `club` (`id`),
+  ADD CONSTRAINT `FK_90D3F060A73F0036` FOREIGN KEY (`ville_id`) REFERENCES `ville` (`id`),
+  ADD CONSTRAINT `FK_90D3F060BCF5E72D` FOREIGN KEY (`categorie_id`) REFERENCES `categorie` (`id`);
+
+--
+-- Contraintes pour la table `adherent_competition`
+--
+ALTER TABLE `adherent_competition`
+  ADD CONSTRAINT `FK_3495E35F25F06C53` FOREIGN KEY (`adherent_id`) REFERENCES `adherent` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_3495E35F7B39D312` FOREIGN KEY (`competition_id`) REFERENCES `competition` (`id`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `club`
+--
+ALTER TABLE `club`
+  ADD CONSTRAINT `FK_B8EE3872A73F0036` FOREIGN KEY (`ville_id`) REFERENCES `ville` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
